fccd315bb33ef47bdc63faf84fad470bc1e38773
Dylan <dylan4047@gmail.com>
2017-01-06 10:39:11 +0100

    changement du lien de home ?page=home en index.php

--- parts/menu.php
+++ parts/menu.php
@@ -3,7 +3,7 @@
 
 	<menu>
 		<ul>
-			<li><a href="/index.php?page=home">Home</a></li>
+			<li><a href="/index.php">Home</a></li>
 			<li><a href="/index.php?page=bio">Bio</a></li>
 			<li><a href="/index.php?page=contact">Contact</a></li>
 		</ul>
